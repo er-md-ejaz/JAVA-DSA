a70556f1b5a9f224f92199ba4dd9932048d195b1
er-md-ejaz <ejazansari0175@gmail.com>
2025-05-10 11:41:39 +0530

    Day-4 commit

---
+++ Day-4/MyFirstProgram.java
@@ -0,0 +1,24 @@
+public class MyFirstProgram {
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        // System.out.println(args[0]+" "+args[1]+" "+"Ansari");
+        // MyFirstClass.main(args);
+        return;
+    }
+    public static void main1(String args[]){
+        System.out.println("Hello Coders!");
+    }
+}
+
+class MyFirstClass {
+    public static void main(String args[]){
+        System.out.println("Hello Programmers!");
+        // MyFirstMG.main(args);
+    }
+}
+
+class MySecondClass {
+    public static void main(String args[]){
+        System.out.println("Hello Developers!");
+    }
+}
