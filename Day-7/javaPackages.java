a2850ce584d2c927cc31df194a0f2d446fb6cc4f
MD Ejaz Ansari <ejazansari0175@gmail.com>
2025-05-15 22:24:16 +0530

    Add files via upload

---
+++ Day-7/javaPackages.java
@@ -0,0 +1,14 @@
+// import java.util.Scanner; // it is used for importing only one class from the package.
+// import java.util.*; // it is used for importing all classes from the package.
+
+
+
+public class javaPackages {
+    // Scanner sc = new Scanner(System.in);
+    
+    // java.util.Scanner sc = new java.util.Scanner(System.in); // 
+    
+    java.util.Date date; 
+    java.sql.Date sqlDate;
+
+}
